26f8ec4878908b9ea421f384f45011eca5c9a713
cmiller36 <m.calliemiller@gmail.com>
2017-04-02 12:41:07 -0400

    Done.

--- src/components/ingredients/AddIngredients.js
+++ src/components/ingredients/AddIngredients.js
@@ -1,13 +1,48 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
+
+import { ConnectedAddIngredient } from './AddIngredient.js'
+import { unselectedIngredients, findIngredientById } from '../../reducers/ingredients'
 
 export class AddIngredients extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  handleOnSubmit(event){
+    event.preventDefault()
+  }
+
   render(){
+    let addIngredients = this.props.unselectedIngredients && this.props.unselectedIngredients.map((ingredient, index) => {
+      return <ConnectedAddIngredient key={index} name={ingredient.name} id={ingredient.id} calories={ingredient.calories} />
+    })
+    let ingredients = this.props.selectedIngredients && this.props.selectedIngredients.map((ingredient) => {
+      return <li> {ingredient.name} </li>
+    })
     return(
       <div>
-          Add More Ingredients
+        <div>
+          Ingredients:
+          {ingredients}
+        </div>
+        <div>
+          Add More Ingredients:
+          {addIngredients}
+        </div>
       </div>
     )
   }
 }
 
-export const ConnectedAddIngredients =  AddIngredients
+function mapStateToProps(state){
+  let selectedIngredients = state.recipeForm.ingredientIds.map(function(ingredientId){
+    return findIngredientById(ingredientId, state.ingredients)
+  })
+  return {ingredients: state.ingredients || [],
+    selectedIngredients: selectedIngredients || [],
+    unselectedIngredients: unselectedIngredients(state.ingredients, state.recipeForm.ingredientIds) || []}
+}
+
+export const ConnectedAddIngredients =  connect(mapStateToProps)(AddIngredients);
+
--- src/components/recipes/RecipesInput.js
+++ src/components/recipes/RecipesInput.js
@@ -1,14 +1,59 @@
 import React, { Component } from 'react';
-
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { addRecipe } from '../../actions/recipes'
+import { AddIngredients } from '../ingredients/AddIngredients'
 
 export class RecipesInput extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: '',
+      calories: ''
+    }
+  }
+
+   handleOnRecipeNameChange(event) {
+    this.setState({
+      name: event.target.value
+    });
+  }
+
+  handleOnCalorieChange(event) {
+    this.setState({
+      calories: event.target.value
+    });
+  }
+
+  handleOnSubmit(event) {
+    event.preventDefault()
+    let recipe = Object.assign({}, this.state, {ingredientIds: this.props.selectedIngredients})
+    this.props.addRecipe(recipe)
+  }
+
+
   render(){
     return(
-      <div>
-        Recipes Input
-      </div>
+      <form onSubmit={this.handleOnSubmit.bind(this)}>
+      <p>
+        <input type="text" onChange={this.handleOnRecipeNameChange.bind(this)} placeholder="Recipe name"/>
+      </p>
+      <p>
+        <input type="text" onChange={this.handleOnCalorieChange.bind(this)} placeholder="calories"/>
+      </p>
+        <AddIngredients />
+        <input type="submit" />
+      </form>
     )
   }
 }
 
-export const ConnectedRecipesInput =(RecipesInput)
+export const ConnectedRecipesInput = connect(mapStateToProps, mapDispatchToProps)(RecipesInput)
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({addRecipe: addRecipe}, dispatch)
+}
+
+function mapStateToProps(state) {
+  return { selectedIngredients: state.recipeForm.ingredientIds}
+}
